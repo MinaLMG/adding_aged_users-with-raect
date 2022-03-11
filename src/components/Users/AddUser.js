46af3f1c17b2a61b61fa5b1e8ec8397dc619d88c
minaLMG <beandro6@gmail.com>
2022-03-12 00:53:13 +0200

    added states for username and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import "./AddUser.css";
 function AddUser() {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredUserAge, setEnteredUserAge] = useState("");
   function addUserHandler(event) {
     event.preventDefault();
+  }
+  function userNameChangeHandler(event) {
+    setEnteredUserName(event.target.value);
+  }
+
+  function userAgeChangeHandler(event) {
+    setEnteredUserAge(event.target.value);
   }
   return (
     <Card class="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">username</label>
-        <input type="text" id="username"></input>
+        <input
+          type="text"
+          id="username"
+          onChange={userNameChangeHandler}
+        ></input>
         <label htmlFor="age">username</label>
-        <input type="number" id="age"></input>
+        <input type="number" id="age" onChange={userAgeChangeHandler}></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>

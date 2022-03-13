a37d91783593f2b2ea2ca14fa5de335956877709
minaLMG <beandro6@gmail.com>
2022-03-14 01:04:55 +0200

    used empty tag

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     });
   }
   return (
-    <Wrapper>
+    <>
       <AddUser onAddUser={addUSerHandler}></AddUser>
       <UsersList users={usersList}></UsersList>
-    </Wrapper>
+    </>
   );
 }
 
